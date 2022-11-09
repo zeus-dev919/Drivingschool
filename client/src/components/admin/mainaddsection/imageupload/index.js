40c484c4ce1c2ff7ee8943e66d20e011d6002001
HadesHappy <happyhades123@gmail.com>
2022-11-09 15:59:03 -0400

    fjk

--- client/src/components/admin/mainaddsection/imageupload/index.js
+++ client/src/components/admin/mainaddsection/imageupload/index.js
@@ -1,33 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import { saveImage } from '../../../../apis/question'
+import { FileUploader } from 'react-drag-drop-files'
 
 const ImageUpload = () => {
+  const fileTypes = ['JPG', 'PNG', 'GIF'];
   const [file, setFile] = useState(null)
+  const [image, setImage] = useState(null)
 
-  const handlePhoto = (e) => {
-    setFile(e.target.files[0])
+  const handleDropChange = (dropFile) => {
+    console.log('dropFile: ', dropFile);
+    setFile(dropFile)
+    setImage(URL.createObjectURL(dropFile));
   }
-  const handleSubmit = () => {
-    if (file) {
-      const formData = new FormData()
-      formData.append('file', file)
-      saveImage(formData)
+
+  useEffect(() => {
+    if (image === null) {
+      setImage('/assets/icons/Main Image.png')
     }
-  }
+    else
+      setImage(image)
+  }, [image])
+
   return (
     <div className='addsection-image-section'>
-      <img src='/assets/icons/Main Image.png' alt='mainImage' style={{ marginBottom: '-70px' }} />
-      <form encType='multipart/form-data'>
-        <img className='uploadbutton-image' src='/assets/icons/Upload Cloud.png' htmlFor='upload' alt='uploadImage' />
-        <div className='upload-button'>
-          <input type='file' accept='.png, .jpg, .jpeg' name='photo' id='upload' onChange={handlePhoto} hidden />
-          <label>Drag and Drop or <label htmlFor='upload' className='browse-text'>Browse</label> to upload</label>
-        </div>
-        <div style={{ cursor: 'pointer' }} onClick={handleSubmit}>Upload</div>
-      </form>
+      <FileUploader
+        handleChange={handleDropChange}
+        name="file"
+        types={fileTypes}
+        children={
+          <div>
+            <img src={image} alt='mainImage' style={{ marginBottom: '-70px' }} />
+            <div>
+              <img className='uploadbutton-image' src='/assets/icons/Upload Cloud.png' htmlFor='upload' alt='uploadImage' />
+              <div className='upload-button'>
+                <div>Drag and Drop or <label htmlFor='upload' className='browse-text'>Browse</label> to upload</div>
+              </div>
+            </div>
+          </div>}
+      />
     </div>
   )
 }
 
 export default ImageUpload
+
+  // < form encType = 'multipart/form-data' >
+
